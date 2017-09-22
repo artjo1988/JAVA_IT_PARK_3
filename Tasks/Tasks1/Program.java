f67d5b3c3b49cd6684e2eb76081e1c44d092c69e
User <email@example.com>
2017-09-22 20:12:56 +0300

    refactor first task

--- Tasks/Tasks1/Program.java
+++ Tasks/Tasks1/Program.java
@@ -1,8 +1,11 @@
+import java.util.Scanner;
 class Program {
-	public static void main(String args[]) {
-		int x = 13;
-		int y = 15;
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		int x = scanner.nextInt();
+		int y = scanner.nextInt();
 		int sumResult = x + y;
+
 		System.out.println(sumResult);
 	}
 }
